03fc68b84c21ff50fc20a65d58a463be56cbbbbd
Prateek Saxena <prtksxna@gmail.com>
2017-02-08 21:03:39 +0530

    Save all the settings

--- options.js
+++ options.js
@@ -1,21 +1,25 @@
 function saveOptions( e ) {
 	e.preventDefault();
 	browser.storage.local.set( {
-		color: document.querySelector( '#color' ).value
+		bgcolor: document.querySelector( '#bgcolor' ).value,
+		textcolor: document.querySelector( '#textcolor' ).value,
+		messages: document.querySelector( '#messages' ).value
 	} );
 };
 
 function restoreOptions() {
 
-	function setCurrentChoice(result) {
-		document.querySelector( '#color' ).value = result.color || 'blue';
+	function setCurrentChoice( result ) {
+		document.querySelector( '#bgcolor' ).value = result.bgcolor || '#ca1f46';
+		document.querySelector( '#textcolor' ).value = result.textcolor || '#ffffff';
+		document.querySelector( '#messages' ).value = result.messages || 'Breathe';
 	};
 
 	function onError( error ) {
 		console.log( `Error: ${error}` );
 	};
 
-	var getting = browser.storage.local.get( 'color' );
+	var getting = browser.storage.local.get( [ 'bgcolor', 'textcolor', 'messages' ] );
 	getting.then( setCurrentChoice, onError );
 }
 
