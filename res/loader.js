f4f275c057152f37855e6eb23945d07509cb2af0
Prateek Saxena <prtksxna@gmail.com>
2017-02-09 18:35:51 +0530

    Load sane settings if there are none

--- res/loader.js
+++ res/loader.js
@@ -1,9 +1,11 @@
 browser.storage.local.get( [ 'bgcolor', 'textcolor', 'messages' ] ).then( function ( r ) {
-	var  m = r.messages.split( '\n' );
+	var  m = r.messages || 'Breathe';
+
+	m = m.split( '\n' );
 	m = m[ Math.floor( Math.random() * m.length ) ];
 
 	document.querySelector( 'h1' ).textContent = m;
-	document.body.style.backgroundColor = r.bgcolor;
-	document.body.style.color = r.textcolor;
+	document.body.style.backgroundColor = r.bgcolor || '#fff';
+	document.body.style.color = r.textcolor || '#b3b3b3';
 	console.log( r );
 } );
