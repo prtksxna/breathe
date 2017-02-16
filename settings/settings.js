3032ab6bdc2449965ca6c966bc1caa1d1c4d9f91
Prateek Saxena <prtksxna@gmail.com>
2017-02-16 19:54:35 +0530

    feat: add a preview in settings
    related to #19

--- settings/settings.js
+++ settings/settings.js
@@ -19,11 +19,38 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	} );
 } );
 
-// Save options
-var settingsInput = document.querySelectorAll( 'input, textarea' );
+// Listen to changes, save settings, and update preview
+var preview = document.querySelector( '#preview' ),
+	settingsInput = document.querySelectorAll( 'input, textarea' );
+
 for ( let input of settingsInput ) {
 	input.addEventListener( 'change', function ( e ) {
 		e.preventDefault();
+
+		var font = document.querySelector( '#font' ).value,
+			messages = document.querySelector( '#messages' ).value.trim(),
+			textcontent = messages.split( '\n' )[ 0 ],
+			textsize = document.querySelector( '#textsize' ).value,
+			textcolor = document.querySelector( '#textcolor' ).value,
+			bgcolor = document.querySelector( '#bgcolor' ).value,
+			bold = document.querySelector( '#bold' ).checked;
+
+		// Update preview
+		if (  font !== '' ) {
+			preview.style.fontFamily = font;
+		}
+
+		if ( bold === false ) {
+			preview.style.fontWeight = 400;
+		}
+
+		preview.textContent = textcontent;
+		preview.style.fontSize = textsize + 'px' || '30px';
+		preview.style.backgroundColor = bgcolor || '#fff';
+		preview.style.color = textcolor || '#b3b3b3';
+
+
+		// Save settings
 		browser.storage.local.set( {
 			bgcolor: document.querySelector( '#bgcolor' ).value,
 			textcolor: document.querySelector( '#textcolor' ).value,
